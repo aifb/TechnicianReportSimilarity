21f365b9fb7b54b7f3ff0778660abbb8349e22da
JannisWalkKIT <jannis.walk@kit.edu>
2018-01-11 15:54:35 +0100

    First findShortestPath method searches only for sameAs relationships.

--- src/main/java/JenaDistance.java
+++ src/main/java/JenaDistance.java
@@ -41,7 +41,7 @@ public class JenaDistance {
         System.out.format("Ontology load time: (%7.2f sec)%n%n", (System.currentTimeMillis() - startTime) / 1000.0);        
         
         String fromSubClassURI = "http://people.aifb.kit.edu/mu2771/step#UnplannedWorkOrder";
-        String toSuperClassURI = "http://people.aifb.kit.edu/mu2771/step#WorkOrder";
+        String toSuperClassURI = "http://people.aifb.kit.edu/mu2771/step#FinishedWorkOrder";
         OntClass fromSubClass = model.getOntClass(fromSubClassURI);
         OntClass toSuperClass = model.getOntClass(toSuperClassURI);
         //OntClass fromSubClass = model.getOntClass("http://people.aifb.kit.edu/mu2771/step#FinishedWorkOrder");
@@ -51,32 +51,46 @@ public class JenaDistance {
         
 //		To be discussed with Sebastian: it works with Filter.any. Relation is rdfs:subclassOf. How to get that into PropertyImpl.
 //        Path path = OntTools.findShortestPath(model, fromSubClass, toSuperClass, Filter.any);
-        Collection <Property> predicates = new ArrayList <Property>();
-        //How to get the predicate subclassOf in? Constructor takes a String uri
-        // Subsequent and rdfs:subClassOf does not work.
-        PropertyImpl subClassOf = new PropertyImpl("http://www.w3.org/2000/01/rdf-schema#subClassOf");
-        PropertyImpl symmetric = new PropertyImpl("http://people.aifb.kit.edu/mu2771/step/symmetricProperty");
-        predicates.add(subClassOf);
-        PredicatesFilter pf = new PredicatesFilter(predicates);
-        Path path = OntTools.findShortestPath(model, fromSubClass, toSuperClass, pf);
-
         
-        if (fromSubClassURI.equals(toSuperClassURI)){
-        	System.out.println("Same node");
+        Collection <Property> sameAsCollection = new ArrayList <Property>();
+        PropertyImpl sameAs = new PropertyImpl("http://www.w3.org/2002/07/owl#sameAs");
+        sameAsCollection.add(sameAs);
+        PredicatesFilter sameAsPf = new PredicatesFilter(sameAsCollection);
+        Path sameAsPath = OntTools.findShortestPath(model, fromSubClass, toSuperClass, sameAsPf);
+        if (sameAsPath != null)
+        {
+        	System.out.println(fromSubClass +" and " + toSuperClass + " are connected by a sameAs relation.");
         }
-        else if (path != null){
-            int superClasses = 0;
-            for (Statement s: path) {
-                if (s.getObject().toString().startsWith(ns)) {
-                    // filter out OWL Classes
-                    superClasses++;
-                    System.out.println(s.getObject());
-                }
+        else
+        {
+            Collection <Property> predicates = new ArrayList <Property>();
+            PropertyImpl subClassOf = new PropertyImpl("http://www.w3.org/2000/01/rdf-schema#subClassOf");
+            PropertyImpl symmetric = new PropertyImpl("http://people.aifb.kit.edu/mu2771/step/symmetricProperty");
+            predicates.add(subClassOf);
+            PredicatesFilter pf = new PredicatesFilter(predicates);
+            Path path = OntTools.findShortestPath(model, fromSubClass, toSuperClass, pf);
+
+            
+            if (fromSubClassURI.equals(toSuperClassURI)){
+            	System.out.println("Same node");
             }
-            System.out.println("Shortest distance from " + fromSubClass + " to " + toSuperClass + " = " + superClasses);
-        }else {
-            System.out.println("No path from " + fromSubClass + " to " + toSuperClass);
+            else if (path != null){
+                int superClasses = 0;
+                for (Statement s: path) {
+                    if (s.getObject().toString().startsWith(ns)) {
+                        // filter out OWL Classes
+                        superClasses++;
+                        System.out.println(s.getObject());
+                    }
+                }
+                System.out.println("Shortest distance from " + fromSubClass + " to " + toSuperClass + " = " + superClasses);
+            }else {
+                System.out.println("No path from " + fromSubClass + " to " + toSuperClass);
+            
+        }
         
+        
+
 //        if (path != null){
 //            int superClasses = 0;
 //            for (Statement s: path) {
