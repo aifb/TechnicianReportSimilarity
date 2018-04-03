700823bed39775c36b909f8ae335c70412807609
sebastian.bader <sebastian.bader@kit.edu>
2018-04-03 17:29:40 +0200

    resolving tfidf handling in python scripts

--- src/main/python/TFIDFDict.py
+++ src/main/python/TFIDFDict.py
@@ -2,15 +2,14 @@ import logging, gensim
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 # load id->word mapping (the dictionary), one of the results of step 2 above
-id2word = gensim.corpora.Dictionary.load_from_text('../../../data/_wordids.txt.bz2')
+id2word = gensim.corpora.Dictionary.load_from_text('../resources/_wordids.txt.bz2')
 # load corpus iterator
 #mm = gensim.corpora.MmCorpus('../../../data/_tfidf.mm')
-mm = gensim.corpora.MmCorpus('../../../data/_tfidf.mm.bz2') # use this if you compressed the TFIDF output (recommended)
+#mm = gensim.corpora.MmCorpus('../../../data/_tfidf.mm.bz2') # use this if you compressed the TFIDF output (recommended)
 
-print(mm)
+#print(mm)
 
-tfidf = gensim.models.TfidfModel.load('../../../data/_tfidf.mm.bz2')
+tfidf = gensim.models.TfidfModel.load('../resources/tfidf.model')
 
 
-tfidf[[(id2word.token2id["zebra"],1),(id2word.token2id["elephant"],1)]]
-print 'Done'
+print tfidf[[(id2word.token2id["zebra"],1),(id2word.token2id["elephant"],1)]]
