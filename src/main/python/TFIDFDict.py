a4e5dc2e455753522ae8d269bbc2800dbab6b8d4
JannisWalkKIT <jannis.walk@kit.edu>
2018-04-05 10:06:07 +0200

    Add idf scores to labels

--- src/main/python/TFIDFDict.py
+++ src/main/python/TFIDFDict.py
@@ -1,4 +1,6 @@
 import logging, gensim
+import pandas as pd
+
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 # load id->word mapping (the dictionary), one of the results of step 2 above
@@ -9,6 +11,8 @@ id2word = gensim.corpora.Dictionary.load_from_text('../resources/_wordids.txt.bz
 
 #print(mm)
 
+#Our idf scores were computed on current Wikipedia corpus (2.4.18 (?))
+
 tfidf = gensim.models.TfidfModel.load('../resources/tfidf.model')
 
 #This was originally used by sba. Does not provide consistent results in the sense that
@@ -18,6 +22,30 @@ tfidf = gensim.models.TfidfModel.load('../resources/tfidf.model')
 
 
 #Looks like here only the actual idf scores are given.
-print tfidf.idfs.get(id2word.token2id["zebra"])
-print tfidf.idfs.get(id2word.token2id["chair"])
+try:
+    print tfidf.idfs.get(id2word.token2id["name"])
+except KeyError:
+    print "The term was not found."
+
+print tfidf.idfs.get(id2word.token2id["car"])
 print tfidf.idfs.get(id2word.token2id["elephant"])
+
+allLabels = pd.read_excel('../../../data/AllLabels.xlsx', header = None)
+allLabels.columns=['label']
+allLabels['idf'] = 0.0
+for index, row in allLabels.iterrows():
+    #allLabels.at[rows,]
+    #print row['label']
+    try:
+        allLabels.at[index, 'idf'] = tfidf.idfs.get(id2word.token2id[row['label'].lower()])
+    except KeyError:
+        allLabels.at[index, 'idf'] = 1.0
+
+print pd.DataFrame.describe(allLabels)
+print pd.DataFrame.info(allLabels)
+
+print pd.Series.value_counts(allLabels['idf'])
+
+writer = pd.ExcelWriter('../../../data/AllLabelsIDF.xlsx')
+allLabels.to_excel(writer,'Sheet1')
+writer.save()
